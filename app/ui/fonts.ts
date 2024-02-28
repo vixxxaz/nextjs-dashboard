3dce6241ffecbe7a5882c38695dee50b92f18dab
vixxxaz <99724809+vixxxaz@users.noreply.github.com>
2024-02-28 07:08:33 +0100

    1er commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { ABeeZee } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
+
+export const aBeeZee = ABeeZee({
+    weight: ['400'],
+    subsets: ['latin'],
+})
